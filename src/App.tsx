9e5b15d560a3e68223f8f96ba06cb27c7238090d
wiib <16376176+wiib@users.noreply.github.com>
2022-11-04 13:45:56 -0500

    add polyfill for Node globals

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
-import type { Component } from "solid-js";
+import { Component, createSignal, JSX } from "solid-js";
 import * as musicMetadata from "music-metadata-browser";
 
 import styles from "./App.module.css";
 import Tag from "./components/Tag";
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles['page-header']}>
-        <h1>Audio Metadata Viewer</h1>
-        <input accept="audio/*" type="file" name="loadFile" id="loadFile" />
-      </header>
-      <hr />
-      <main class={styles['page-content']}>
-        <div class={`${styles['page-content__item']} ${styles['covers-container']}`}>
-          <h1>Covers</h1>
-          <img
-            src="https://pbs.twimg.com/media/E6rSmNPXsAMW5ca.jpg:large"
-            alt=""
-          />
-          <div>
-            {/* put tags here */}
-          </div>
+    const [metadata, setMetadata] = createSignal<musicMetadata.IAudioMetadata | null>(null);
+
+    const onLoadFile: JSX.EventHandler<HTMLInputElement, Event> = (event) => {
+        const file = event.currentTarget.files?.item(0) as Blob;
+        musicMetadata.parseBlob(file).then((meta) => {
+            setMetadata(meta);
+            console.log(metadata());
+        });
+    };
+
+    return (
+        <div class={styles.App}>
+            <header class={styles["page-header"]}>
+                <h1>Audio Metadata Viewer</h1>
+                <input
+                    accept="audio/*"
+                    type="file"
+                    name="loadFile"
+                    id="loadFile"
+                    onChange={onLoadFile}
+                />
+            </header>
+            <hr />
+            <main class={styles["page-content"]}>
+                <div class={`${styles["page-content__item"]} ${styles["covers-container"]}`}>
+                    <h1>Covers</h1>
+                    <img src="https://pbs.twimg.com/media/E6rSmNPXsAMW5ca.jpg:large" alt="" />
+                    <div>{/* put tags here */}</div>
+                </div>
+                <hr />
+                <div class={`${styles["page-content__item"]} ${styles["tags-container"]}`}>
+                    <h1>Tags</h1>
+                    <div>{/* put tags here */}</div>
+                </div>
+            </main>
         </div>
-        <hr />
-        <div class={`${styles['page-content__item']} ${styles['tags-container']}`}>
-          <h1>Tags</h1>
-          <div>
-            {/* put tags here */}
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+    );
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+    plugins: [solidPlugin()],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: "esnext",
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: "globalThis"
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true,
+                    process: true
+                })
+            ]
+        }
+    },
+    resolve: {
+        alias: {
+            process: "process/browser",
+            util: "util"
+        }
+    }
 });
