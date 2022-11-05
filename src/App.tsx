475bea8c8dec50dacd715934484c5a3a687ec0af
wiib <16376176+wiib@users.noreply.github.com>
2022-11-05 14:16:42 -0500

    implement basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
-import { Component, createSignal, JSX } from "solid-js";
+import { Component, createSignal, JSX, Show, For } from "solid-js";
 import * as musicMetadata from "music-metadata-browser";
 
 import styles from "./App.module.css";
 import Tag from "./components/Tag";
 
+interface IObject {
+    [key: string]: any;
+}
+
+interface ISeparatedTags {
+    common: IObject;
+    format: IObject;
+    native: IObject;
+}
+
 const App: Component = () => {
     const [metadata, setMetadata] = createSignal<musicMetadata.IAudioMetadata | null>(null);
+
+    const flattenObject = (object: IObject, newObject: IObject, prevKey: string) => {
+        for (let key in object) {
+            let value = object[key];
+
+            if (key == "picture") continue;
+
+            if (value && typeof value !== "object" && !Array.isArray(value)) {
+                if (prevKey == null || prevKey == "") {
+                    newObject[key] = value;
+                } else {
+                    if (key == null || key == "") {
+                        newObject[prevKey] = value;
+                    } else {
+                        newObject[`${prevKey}.${key}`] = value;
+                    }
+                }
+            } else {
+                if (prevKey == null || prevKey == "") {
+                    flattenObject(value, newObject, key);
+                } else {
+                    flattenObject(value, newObject, `${prevKey}.${key}`);
+                }
+            }
+        }
+    };
+
+    const parseObject = (object: IObject) => {
+        let newObject: IObject = {};
+
+        flattenObject(object, newObject, "");
+
+        return Object.entries(newObject).map((value, index) => {
+            return <Tag name={value[0]} data={value[1]} />;
+        });
+    };
 
     const onLoadFile: JSX.EventHandler<HTMLInputElement, Event> = (event) => {
         const file = event.currentTarget.files?.item(0) as Blob;
@@ -31,13 +77,25 @@ const App: Component = () => {
             <main class={styles["page-content"]}>
                 <div class={`${styles["page-content__item"]} ${styles["covers-container"]}`}>
                     <h1>Covers</h1>
-                    <img src="https://pbs.twimg.com/media/E6rSmNPXsAMW5ca.jpg:large" alt="" />
                     <div>{/* put tags here */}</div>
                 </div>
                 <hr />
                 <div class={`${styles["page-content__item"]} ${styles["tags-container"]}`}>
                     <h1>Tags</h1>
-                    <div>{/* put tags here */}</div>
+                    <Show when={metadata()}>
+                        <h2>Common Tags</h2>
+                        <div>
+                            <For each={parseObject(metadata()!.common)}>{(item) => item}</For>
+                        </div>
+                        <h2>Format Tags</h2>
+                        <div>
+                            <For each={parseObject(metadata()!.format)}>{(item) => item}</For>
+                        </div>
+                        <h2>Native Tags</h2>
+                        <div>
+                            {/* <For each={parseObject(metadata()!.native)}>{(item) => item}</For> */}
+                        </div>
+                    </Show>
                 </div>
             </main>
         </div>
