c4bcbf26db7f8dd51c66d53bf92a20eb9dd4cafb
wiib <16376176+wiib@users.noreply.github.com>
2022-11-06 16:38:39 -0500

    finally solve the node polyfills problem

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,17 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
     plugins: [solidPlugin()],
-    server: {
-        port: 3000,
+    resolve: {
+        alias: {
+            process: "process/browser",
+        },
     },
     build: {
         target: "esnext",
     },
-    optimizeDeps: {
-        esbuildOptions: {
-            define: {
-                global: "globalThis"
-            },
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true,
-                    process: true
-                })
-            ]
-        }
+    server: {
+        port: 3000,
     },
-    resolve: {
-        alias: {
-            process: "process/browser",
-            util: "util"
-        }
-    }
 });
