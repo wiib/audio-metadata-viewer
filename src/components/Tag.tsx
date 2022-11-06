c5dd57961dda481649cc1bc239f82ec037c78034
wiib <16376176+wiib@users.noreply.github.com>
2022-11-06 12:12:05 -0500

    make Tag render its children instead of a prop

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,17 +1,20 @@
 import type { Component } from "solid-js";
+import { JSX, children } from "solid-js";
 
 import classes from "./Tag.module.css";
 
 interface IProps {
     name: string;
-    data: string;
+    children?: JSX.Element;
 }
 
 const Tag: Component<IProps> = (props: IProps) => {
+    const c = children(() => props.children);
+
     return (
         <div class={classes.Tag}>
-            <span class="name">{props.name}</span>
-            <span class="data">{props.data}</span>
+            <span>{props.name}</span>
+            <div class={classes.Tag__data}>{c()}</div>
         </div>
     );
 };
